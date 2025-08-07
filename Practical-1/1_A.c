37ada3cf5c7937ddee8081785df730c73cc7c747
Nirali276 <niralibhavsar68@gmail.com>
2025-08-07 09:01:42 +0530

    Update and rename 1_a.c to 1_A.c

---
+++ Practical-1/1_A.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 15, 20, 25, 30};
+    int i;
+
+    printf("Array elements:\n");
+    for(i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
